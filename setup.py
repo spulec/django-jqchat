0ed500e2d22c6d5a8fb36e91206c004ac264aff7
Richard Barran <richard@arbee-design.co.uk>
2013-02-03 03:57:08 +0000

    Add setup.py so that we can install code as a package.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='django-jqchat',
+    version='0.1',
+    packages=['jqchat'],
+    author='Richard Barran',
+    long_description=open('README.rst').read(),
+    url="https://github.com/richardbarran/django-jqchat",
+)
