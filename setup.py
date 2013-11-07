c27ad6dbe1755e7c5e645e87c8bfbef5327feade
Steve Pulec <spulec@gmail.com>
2013-11-07 14:10:00 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-jqchat',
-    version='0.1',
+    version='0.1.1',
     packages=['jqchat'],
     author='Richard Barran',
     include_package_data=True,
